fb1ca90a4a08b97417d77f657cfe0854aed375ed
Guilherme Sousa <sousadominguesgui@hotmail.com>
2026-05-20 10:46:19 -0300

    feat: adiciona match case e listas vazias

--- fluxo-norte.py
+++ fluxo-norte.py
@@ -1,2 +1,8 @@
+pedidos = []
+entregadores = []
+
 opcao = int(input("Digite a opção desejada: \n\n1 - Cadastro de pedidos \n2 - Cadastro de Entregadores \n3 - Atualização dos pedidos \n4 - Consulta de informações \n5 - Relatórios operacionais \n6 - Finalizar o sistema\n\n"))
 
+match(opcao):
+    case 1:
+        
