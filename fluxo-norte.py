d6e0ed97949b2c3d36157cd71feb18e4d3a5361c
thelipesousa <feltista07123@gmail.com>
2026-05-20 10:40:57 -0300

    feat: adiciona menu de opções para o usuário

---
+++ fluxo-norte.py
@@ -0,0 +1,2 @@
+opcao = int(input("Digite a opção desejada: \n\n1 - Cadastro de pedidos \n2 - Cadastro de Entregadores \n3 - Atualização dos pedidos \n4 - Consulta de informações \n5 - Relatórios operacionais \n6 - Finalizar o sistema\n\n"))
+
